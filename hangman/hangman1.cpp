22d3d7453f763d717fe1bca08b4a5d846f116773
chadamine <chadwicklew@gmail.com>
2013-05-19 14:20:27 -0600

    hangman1.cpp

---
+++ hangman/hangman1.cpp
@@ -0,0 +1,78 @@
+/*
+Chad Lewis
+CSC 160-001
+Hangman
+Output sequential series of hangman phases
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "    -------|" << endl;
+	cout << "    |      |" << endl;
+	cout << "           |" << endl;
+	cout << "           |" << endl;
+	cout << "           |" << endl;
+	cout << "           |" << endl;
+	cout << "         _____" << endl;
+	cout << endl << endl;
+
+	cout << "    -------|" << endl;
+	cout << "    |      |" << endl;
+	cout << "    O      |" << endl;
+	cout << "           |" << endl;
+	cout << "           |" << endl;
+	cout << "           |" << endl;
+	cout << "         _____" << endl;
+	cout << endl << endl;
+
+	cout << "    -------|" << endl;
+	cout << "    |      |" << endl;
+	cout << "    O      |" << endl;
+	cout << "    |      |" << endl;
+	cout << "           |" << endl;
+	cout << "           |" << endl;
+	cout << "         _____" << endl;
+	cout << endl << endl;
+
+	cout << "    -------|" << endl;
+	cout << "    |      |" << endl;
+	cout << "    O      |" << endl;
+	cout << "   -|      |" << endl;
+	cout << "           |" << endl;
+	cout << "           |" << endl;
+	cout << "         _____" << endl;
+	cout << endl << endl;
+
+	cout << "    -------|" << endl;
+	cout << "    |      |" << endl;
+	cout << "    O      |" << endl;
+	cout << "   -|-     |" << endl;
+	cout << "           |" << endl;
+	cout << "           |" << endl;
+	cout << "         _____" << endl;
+	cout << endl << endl;
+
+	cout << "    -------|" << endl;
+	cout << "    |      |" << endl;
+	cout << "    O      |" << endl;
+	cout << "   -|-     |" << endl;
+	cout << "   /       |" << endl;
+	cout << "           |" << endl;
+	cout << "         _____" << endl;
+	cout << endl << endl;
+
+	cout << "    -------|" << endl;
+	cout << "    |      |" << endl;
+	cout << "    O      |" << endl;
+	cout << "   -|-     |" << endl;
+	cout << "   / \\     |" << endl;
+	cout << "           |" << endl;
+	cout << "         _____" << endl;
+	cout << endl << endl;
+
+	system("pause");
+	return 0;
+}
